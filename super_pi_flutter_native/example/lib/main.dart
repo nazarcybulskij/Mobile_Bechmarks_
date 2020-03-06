8ddad85ec767eb1e54f6f3fea79fd71a2925b88b
Nazar Tsybulskyi <nazar.tsybulskyi@inveritasoft.com>
2020-03-06 17:27:48 +0200

    added  FFI

--- super_pi_flutter_native/example/lib/main.dart
+++ super_pi_flutter_native/example/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'dart:ffi';  // For FFI
 import 'dart:io';   // For Platform.isX
+import 'common.dart';
+
+
+const int _kNumIters = 100;
 
 final DynamicLibrary nativeAddLib =
 Platform.isAndroid
@@ -8,36 +12,62 @@ Platform.isAndroid
     : DynamicLibrary.process();
 
 void main() {
-  runApp(MyApp());
+  runApp(Home());
 }
 
-class MyApp extends StatefulWidget {
+class Home extends StatefulWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _HomeState extends State<Home> {
 
-  final int Function(int x, int y) nativeAdd =
-  nativeAddLib
-      .lookup<NativeFunction<Int32 Function(Int32, Int32)>>("native_add")
+  String time = '';
+
+  final double Function(int) getOneByPi =  nativeAddLib
+      .lookup<NativeFunction<Double Function(Int32)>>("getOneByPi")
       .asFunction();
 
+  final double Function(int) gaussLegendre =  nativeAddLib
+      .lookup<NativeFunction<Double Function(Int32)>>("gaussLegendre")
+      .asFunction();
 
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('1 + 2 == ${nativeAdd(1, 2)}'),
+        body: Container(
+          child: Center(
+            child: RaisedButton(
+              child: Text(time),
+              onPressed: (){
+                //var startdate = DateTime.now().millisecondsSinceEpoch;
+                final Stopwatch watch = Stopwatch();
+                print('Pi calc tracker benchmark...');
+                watch.start();
+                for (int i = 0; i < _kNumIters; i += 1) {
+                  gaussLegendre(100000000);
+                  //getOneByPi(1000000);
+                }
+                watch.stop();
+                //var finishDate = DateTime.now().millisecondsSinceEpoch;
+                final BenchmarkResultPrinter printer = BenchmarkResultPrinter();
+                printer.addResult(
+                  description: 'Pi calc tracker',
+                  value: watch.elapsedMicroseconds / _kNumIters,
+                  //value: (finishDate - startdate)/_kNumIters,
+                  unit: 'µs per iteration',
+                  name: 'iteration',
+                );
+                printer.printToStdout();
+                setState(() {
+                  time ='${watch.elapsedMicroseconds / _kNumIters}';
+                  //time ='${(finishDate - startdate)/_kNumIters}';
+                });
+              },
+            ),
+          ),
         ),
       ),
     );
--- super_pi_flutter_native/ios/Classes/super_pi_flutter_native.cpp
+++ super_pi_flutter_native/ios/Classes/super_pi_flutter_native.cpp
@@ -1,6 +1,43 @@
 #include <stdint.h>
+#include <math.h>
 
 extern "C" __attribute__((visibility("default"))) __attribute__((used))
 int32_t native_add(int32_t x, int32_t y) {
     return x + y;
 }
+
+
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
+double gaussLegendre(int32_t iterations) {
+    double a = 1.0;
+    double b = 1.0 / sqrt(2);
+    double t = 1.0 / 4.0;
+    double p = 1.0;
+
+    for (int i = 0; i < iterations; i++) {
+      double aNext = (a + b) / 2;
+      double bNext = sqrt(a * b);
+      double tNext = t - p * pow(a - aNext, 2);
+      double pNext = 2 * p;
+      a = aNext;
+      b = bNext;
+      t = tNext;
+      p = pNext;
+    }
+    return pow(a + b, 2) / (4 * t);
+}
+
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
+double getOneByPi(int32_t k) {
+    double ak = 6.0 - 4 * sqrt(2);
+    double yk = sqrt(2) - 1.0;
+    double ak1 ;
+    double yk1 ;
+    for (int i = 0; i < k; i++) {
+      yk1 = (1 - pow((1 - yk * yk * yk * yk),(0.25)))/(1 + pow((1 - yk * yk * yk * yk),(0.25)));
+      ak1 = ak * pow((1 + yk1), 4) - pow(2, 2 * i + 3) * yk1 * (1 + yk1 + yk1 * yk1);
+      yk = yk1;
+      ak = ak1;
+    }
+    return ak;
+ }
